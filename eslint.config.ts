e66f4ac604836e7af62f337230e3eecc469565f6
Chieftain SavageTribe <chieftain@savage-tribe.com>
2026-01-16 02:38:18 +0900

    chore: add ESLint and Prettier configuration

---
+++ eslint.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'eslint/config';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default defineConfig([
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  eslintConfigPrettier,
+]);
